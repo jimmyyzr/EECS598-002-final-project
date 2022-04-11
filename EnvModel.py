5466302a8a24d643ccdddc6f07e3329c48bd4199
LINWU <yatinlin@umich.edu>
2022-04-11 00:59:30 -0400

    GP model-based Training

---
+++ EnvModel.py
@@ -0,0 +1,33 @@
+import numpy as np
+from GP import gp_train, gp_eval
+
+
+class EnvModel:
+    """Similar to the memory buffer in DQN, you can store past experiences in here.
+    Alternatively, the model can generate next state and reward signal accurately."""
+    def __init__(self):
+        self.data_x = []
+        self.data_y = []
+        self.model = None
+
+    def store_data(self, s, a, r, s_next):
+        
+        x = np.concatenate((s, a))
+        self.data_x.append(x.tolist())
+        r = np.array([r])
+        y = np.concatenate((r, s_next))
+        self.data_y.append(y.tolist())
+        
+
+    def update_model(self):
+        train_x = np.array(self.data_x)
+        train_y = np.array(self.data_y)
+        self.model = gp_train(train_x, train_y)
+
+    def generate_data(self, s, a):
+        test_x = np.concatenate((s, a)).reshape(1,-1)
+        model,likelihood,scale = self.model[:]
+        predic =  gp_eval(test_x, model, likelihood, scale)
+        r = predic[0]
+        s_next = predic[1:-1]
+        return r, s_next
---
+++ test.py
@@ -0,0 +1,25 @@
+import gym
+from gym.spaces import Discrete, Dict, Box
+import os
+import numpy as np
+from EnvModel import EnvModel
+os.environ['LANG']='en_US'
+
+env = gym.make("FetchReach-v1")
+env_sim = EnvModel()
+state = env.reset()
+for _ in range(1000):
+  env.render()
+  action = env.action_space.sample() # your agent here (this takes random actions)
+  next_state, reward, done, info = env.step(action)
+  env_sim.store_data(state['observation'],action,reward,next_state['observation'])
+  if _ > 10:
+    env_sim.update_model()
+    r,s_next = env_sim.generate_data(state['observation'],action)
+  state = next_state
+  if done:
+    observation = env.reset()
+env.close()
+
+
+
